88e4cf4b148f05967273b06b63bb53a86b6eec4e
Alex Yegian <ayegian@unix1.lt.ucsc.edu>
2021-04-28 22:39:45 -0700

    new

--- code/commands.cpp
+++ code/commands.cpp
@@ -43,6 +43,15 @@ int exit_status_message() {
 
 void fn_cat (inode_state& state, const wordvec& words) {
    DEBUGF ('c', state);
+   //NAVIGATION ASSIGN SUPPOSED TO BE TO INODE PTR NOT CWD
+   inode_ptr ptr = state.cwd;
+   if (ptr->contents->type == file_type::PLAIN_TYPE) {
+       plain_file* a = static_cast<plain_file*>(state.cwd->contents.get());
+       const wordvec& b = a->readfile();
+       for (size_t i = 0; i < b.size(); i++) {
+           printf("%s ",b[i].c_str());
+       }
+   }
    DEBUGF ('c', words);
 }
 
@@ -91,58 +100,98 @@ void fn_exit (inode_state& state, const wordvec& words) {
    throw ysh_exit();
 }
 
-void fn_ls (inode_state& state, const wordvec& words) {
-   DEBUGF ('c', state);
-   inode_ptr start_ptr = state.get_cwd();
-   if (words.size() > 1) {
-       printf("WORDS 1: %s\n", words[1].c_str());
+void fn_ls(inode_state& state, const wordvec& words) {
+    DEBUGF('c', state);
+    inode_ptr stateHold = state.cwd;
+    if (words.size() > 1) {
+        printf("WORDS 1: %s\n", words[1].c_str());
+        wordvec words2 = split(words[1], "/\t");
+        std::map<string, inode_ptr>::iterator it;
+        for (int i = 0; static_cast<unsigned long>(i) < words2.size(); i++)
+        {
+            cout << "PARSED WORD: " << words2[i] << endl;
+            directory* a = static_cast<directory*>(state.cwd->contents.get());
+            if (a->dirents.find(words2[i]) == a->dirents.end())
+            {
+                //throw error
+                return;
+            }
+            it = a->dirents.find(words2[i]);
+            state.cwd = it->second;
+        }
+    }
 
-       // dir1/2/3
-       // dir1 2 3
-       //NAVIGATION GOES HERE
-
-       //CHANGE DIRECTORY TO DESTINATION, DO THIS, THEN CHANGE CWD BACK TO OLD ONE
-   }
-
-   directory* dir = static_cast<directory*>(state.get_cwd()->contents.get());
-   dir->list_dirents();
-   state.cwd = start_ptr;
-   DEBUGF ('c', words);
+    directory* dir = static_cast<directory*>(state.get_cwd()->contents.get());
+    dir->list_dirents();
+    DEBUGF('c', words);
+    state.cwd = stateHold;
 }
+
 
 void fn_lsr (inode_state& state, const wordvec& words) {
    DEBUGF ('c', state);
    DEBUGF ('c', words);
 }
 
-void fn_make (inode_state& state, const wordvec& words) {
-   DEBUGF ('c', state);
-   directory* a = static_cast<directory*>(state.cwd->contents.get());
-   if (words.size() > 2) {
-       //PROVIDED INIT VAL
-       wordvec vec;
-       for (size_t i = 2; i < words.size(); i++) {
-           vec.push_back(words[i]);
-       }
-       inode_ptr ptr = a->mkfile(words[1]);
-       if (ptr == nullptr) {
-           return;
-       }
-       plain_file* new_file = static_cast<plain_file*>(ptr->contents.get());
-       new_file->writefile(vec);
-   }
-   else {
-       a->mkfile(words.back());
-   }
-   DEBUGF ('c', words);
+void fn_make(inode_state& state, const wordvec& words) {
+    DEBUGF('c', state);
+    wordvec words2 = split(words[1], "/\t");
+    inode_ptr stateHold = state.cwd;
+    std::map<string, inode_ptr>::iterator it;
+    for (int i = 0; static_cast<unsigned long>(i) < words2.size() - 1; i++)
+    {
+        cout << "PARSED WORD: " << words2[i] << endl;
+        directory* a = static_cast<directory*>(state.cwd->contents.get());
+        if (a->dirents.find(words2[i]) == a->dirents.end())
+        {
+            //throw error
+            return;
+        }
+        it = a->dirents.find(words2[i]);
+        state.cwd = it->second;
+    }
+    directory* a = static_cast<directory*>(state.cwd->contents.get());
+    if (words.size() > 2) {
+        //PROVIDED INIT VAL
+        wordvec vec;
+        for (size_t i = 2; i < words.size(); i++) {
+            vec.push_back(words[i]);
+        }
+        inode_ptr ptr = a->mkfile(words2.back());
+        if (ptr == nullptr) {
+            return;
+        }
+        plain_file* new_file = static_cast<plain_file*>(ptr->contents.get());
+        new_file->writefile(vec);
+    }
+    else {
+        a->mkfile(words2.back());
+    }
+    DEBUGF('c', words);
+    state.cwd = stateHold;
 }
 
-void fn_mkdir (inode_state& state, const wordvec& words) {
-   DEBUGF ('c', state);
-   directory* a = static_cast<directory*>(state.cwd->contents.get());
-   printf("%s\n", words.back().c_str());
-   a->mkdir(words.back());
-   DEBUGF ('c', words);
+void fn_mkdir(inode_state& state, const wordvec& words) {
+    DEBUGF('c', state);
+    wordvec words2 = split(words[1], "/\t");
+    inode_ptr stateHold = state.cwd;
+    std::map<string, inode_ptr>::iterator it;
+    for (int i = 0; static_cast<unsigned long>(i) < words2.size() - 1; i++)
+    {
+        cout << "PARSED WORD: " << words2[i] << endl;
+        directory* a = static_cast<directory*>(state.cwd->contents.get());
+        if (a->dirents.find(words2[i]) == a->dirents.end())
+        {
+            //throw error
+            return;
+        }
+        it = a->dirents.find(words2[i]);
+        state.cwd = it->second;
+    }
+    directory* a = static_cast<directory*>(state.cwd->contents.get());
+    printf("%s\n", words.back().c_str());
+    a->mkdir(words2.back());
+    state.cwd = stateHold;
 }
 
 void fn_prompt (inode_state& state, const wordvec& words) {
@@ -171,7 +220,6 @@ void fn_pwd(inode_state& state, const wordvec& words) {
 
     }
 }
-
 void fn_rm (inode_state& state, const wordvec& words) {
    DEBUGF ('c', state);
    directory* a = static_cast<directory*>(state.cwd->contents.get());
